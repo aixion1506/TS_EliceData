8c6829692ce7eb7150abdad80ef88eba068f879a
aixion1506 <aixion1506@gmail.com>
2023-05-24 21:21:48 +0900

    Feat/Main

---
+++ Bike-Buddy/src/pages/MainPage/Main.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  MainWrapper,
+  BannerContainer1,
+  BannerContainer2,
+  BannerContainer3,
+  DescWrapper,
+} from './Main-styled';
+
+// import { Link } from "react-router-dom";
+
+const Main: React.FC = () => {
+  return (
+    <MainWrapper>
+      <BannerContainer1>
+        <div>
+          <p>공공자전거,</p>
+          <p> 이용해 본 경험이 있으신가요?</p>
+        </div>
+      </BannerContainer1>
+      <BannerContainer2>
+        <DescWrapper>
+          <p>&quot;누구나, 언제나, 어디서나 &quot;</p>
+          <p>쉽고 편리하게 이용할 수 있는</p>
+          <span>공공자전거</span>
+          <div>
+            <p>매년 이용건수가</p>
+            <p>증가하는 추세를 보이며</p>
+            <p>교통수단 중 하나로 자리잡았습니다.</p>
+          </div>
+        </DescWrapper>
+      </BannerContainer2>
+      <BannerContainer3>
+        <DescWrapper>
+          <span>시간대별 공공자전거 이용건수</span>
+          <div>
+            <p>&quot;평일 출퇴근시간&quot;에</p>
+            <p>가장 많은 이용률을 보입니다.</p>
+          </div>
+          <p>한산한 주말 출퇴근시간대,</p>
+          <p>모임을 통해 하루를 새롭게 시작하거나</p>
+          <p>마무리 해보는 건 어떨까요?</p>
+          {/* <GotoGathering>
+                <Link to={ROUTE.GATHERING_LIST.link}>모임하러 가기</Link>
+              </GotoGathering> */}
+        </DescWrapper>
+      </BannerContainer3>
+    </MainWrapper>
+  );
+};
+
+export default Main;
